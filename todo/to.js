d27210e0262b1b9d6307605c0bc238919f2254bd
ANNAPOORNA V <139335974+Annapoornaaradhya@users.noreply.github.com>
2023-11-27 19:45:40 +0530

    18/11/2023

---
+++ todo/to.js
@@ -0,0 +1,38 @@
+const inputBox = document.getElementById("inputbox");
+const listContainer = document.getElementById("listcontainer");
+function addTask() {
+  if (inputBox.value === "") {
+    alert("You Must Write Something!");
+  } else {
+    let li = document.createElement("li");
+    li.innerHTML = inputBox.value;
+    listContainer.appendChild(li);
+    let span = document.createElement("span");
+    span.innerHTML = "\u00d7";
+    li.appendChild(span);
+  }
+  inputBox.value = "";
+  saveData();
+}
+
+listContainer.addEventListener(
+  "click",
+  function (e) {
+    if (e.target.tagName === "LI") {
+      e.target.classList.toggle("checked");
+    } else if (e.target.tagName === "SPAN") {
+      e.target.parentElement.remove();
+      saveData();
+    }
+  },
+  false
+);
+
+function saveData() {
+  localStorage.setItem("data", listContainer.innerHTML);
+}
+
+function showTask() {
+  listContainer.innerHTML = localStorage.getItem("data");
+}
+showTask();
